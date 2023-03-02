59d369ed9a6d2c55654349fd06ad47a3785c1e16
Gerald Diaz <dave.809@hotmail.com>
2023-03-01 12:56:09 -1200

    Luchar

--- src/app/views/luchar/luchar.component.ts
+++ src/app/views/luchar/luchar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-luchar',
   templateUrl: './luchar.component.html',
-  styleUrls: ['./luchar.component.scss']
+  styleUrls: ['./luchar.component.scss', './styleL.component.css']
 })
 export class LucharComponent {
 
